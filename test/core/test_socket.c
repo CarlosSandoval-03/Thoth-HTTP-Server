6c158ec3ac8ac47bec7bf7023d655285f4381147
Carlos Sandoval <csandovalc@unal.edu.co>
2024-06-28 13:07:54 -0500

    test(network): :test_tube: add socket test functions
    The code changes include the addition of test functions for socket creation, binding, listening, accepting, and connecting. These functions are implemented to handle sockets in a basic API.
    
    Issue: Is necessary solve the blocking call in accept socket test.
    
    Signed-off-by: Carlos Sandoval <csandovalc@unal.edu.co>

--- test/core/test_socket.c
+++ test/core/test_socket.c
@@ -23,9 +23,81 @@ void test_create_socket(void)
 	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, 0);
 }
 
+void test_bind_socket(void)
+{
+	int sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
+	TEST_ASSERT_TRUE(sockfd > 0);
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, bind_socket(sockfd, &addr));
+
+	close_socket(sockfd);
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, 0);
+}
+
+void test_listen_socket(void)
+{
+	int sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
+	TEST_ASSERT_TRUE(sockfd > 0);
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, bind_socket(sockfd, &addr));
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, listen_socket(sockfd, 5));
+
+	close_socket(sockfd);
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, 0);
+}
+
+void test_accept_socket(void)
+{
+	int sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
+	TEST_ASSERT_TRUE(sockfd > 0);
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, bind_socket(sockfd, &addr));
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, listen_socket(sockfd, 5));
+
+	int new_sockfd = accept_socket(sockfd, &addr);
+	TEST_ASSERT_TRUE(new_sockfd > 0);
+
+	close_socket(sockfd);
+	close_socket(new_sockfd);
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, 0);
+}
+
+void test_connect_socket(void)
+{
+	int sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
+	TEST_ASSERT_TRUE(sockfd > 0);
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	TEST_ASSERT_EQUAL_INT(TH_SOCKET_SUCCESS, connect_socket(sockfd, &addr));
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
 	RUN_TEST(test_create_socket);
+	RUN_TEST(test_bind_socket);
+	RUN_TEST(test_listen_socket);
+	// RUN_TEST(test_accept_socket);
+	RUN_TEST(test_connect_socket);
+
 	return UNITY_END();
 }
